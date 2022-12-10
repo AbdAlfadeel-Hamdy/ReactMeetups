0e49c8dce0da6b679570b618865a31d7d66794dd
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-12-10 07:11:51 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Switch, Route } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
