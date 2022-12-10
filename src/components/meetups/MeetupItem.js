c189ef492e1c9fd2aa469131d2ae9d59bdfbc1a9
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2022-12-10 07:39:47 +0200

    adding meetups

---
+++ src/components/meetups/MeetupItem.js
@@ -0,0 +1,20 @@
+import classes from "./MeetupItem.module.css";
+const MeetupItem = ({ image, title, address, description }) => {
+  return (
+    <li className={classes.item}>
+      <div className={classes.image}>
+        <img src={image} alt={title} />
+      </div>
+      <div className={classes.content}>
+        <h3>{title}</h3>
+        <address>{address}</address>
+        <p>{description}</p>
+      </div>
+      <div className={classes.actions}>
+        <button>Add to favorites</button>
+      </div>
+    </li>
+  );
+};
+
+export default MeetupItem;
